7cedb6c60f37370e978e5e002a8b4492151ea934
Hiroyuki Sato <sathiroyuki@gmail.com>
2015-06-03 21:56:20 +0900

    specを追加

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -2,9 +2,13 @@ require File.expand_path(File.dirname(__FILE__) + '/../triangle')
 
 describe Triangle do
   specify { expect(Triangle.new(0, 1, 2).category).to eq :invalid }
-  specify { expect(Triangle.new(1, 1, 2).category).to eq :invalid }
-  specify { expect(Triangle.new(1, 1, 1).category).to eq :regular }
-  specify { expect(Triangle.new(2, 2, 3).category).to eq :isosceles }
-  specify { expect(Triangle.new(2, 3, 3).category).to eq :isosceles }
+  specify { expect(Triangle.new(1, 2, 3).category).to eq :invalid }
   specify { expect(Triangle.new(2, 3, 4).category).to eq :scalene }
+  specify { expect(Triangle.new(1, 1, 2).category).to eq :invalid }
+  specify { expect(Triangle.new(2, 2, 3).category).to eq :isosceles }
+  specify { expect(Triangle.new(2, 2, 4).category).to eq :invalid }
+  specify { expect(Triangle.new(3, 3, 4).category).to eq :isosceles }
+  specify { expect(Triangle.new(0, 0, 0).category).to eq :invalid }
+  specify { expect(Triangle.new(1, 1, 1).category).to eq :regular }
+  specify { expect(Triangle.new(2, 2, 2).category).to eq :regular }
 end
